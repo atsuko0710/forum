a69e1f5304d8b6635d7d307bd7d59d8d402fbe12
atsukodan <atsukodan10@gmail>
2020-11-16 22:35:28 +0800

    class 2

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ Route::get('/', function () {
 });
 
 Route::get('/threads', 'ThreadsController@index');
+Route::get('/thread/{thread}', 'ThreadsController@show');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -3,23 +3,30 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ThreadsTest extends TestCase
 {
     use DatabaseMigrations;
 
     /**
-     * A basic test example.
-     *
-     * @return void
+     * @test
      */
     public function a_user_can_browse_threads()
     {
+        $thread = factory('App\Thread')->create();
         $response = $this->get('/threads');
 
-        $response->assertStatus(200);
+        $response->assertSee($thread->title);
+    }
+
+    /**
+     * @test
+     */
+    public function a_user_can_read_a_single_thread()
+    {
+        $thread = factory('App\Thread')->create();
+        $response = $this->get('/thread/'.$thread->id);
+        $response->assertSee($thread->title);
     }
 }
