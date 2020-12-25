8f7f52ca5a6fdf6f5b3e5a9f5624e86d1085b2d5
lincy <lincy@wecut.com>
2020-12-25 11:50:13 +0800

    重构点赞功能

--- app/Favorite.php
+++ app/Favorite.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Favorite extends Model
 {
     protected $guarded = [];
+
+    public function favorited()
+    {
+        return $this->morphTo();
+    }
 }
--- app/Reply.php
+++ app/Reply.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    use RecordsActivity;
+    use RecordsActivity, Favoritable;
 
     protected $guarded = [];
     protected $with = ['owner', 'favorites'];
@@ -16,37 +16,37 @@ class Reply extends Model
         return $this->belongsTo(User::class, 'user_id', 'id');
     }
 
-    public function favorites()
-    {
-        return $this->morphMany(Favorite::class, 'favorited');
-    }
+    // public function favorites()
+    // {
+    //     return $this->morphMany(Favorite::class, 'favorited');
+    // }
 
-    public function getFavoritesCountAttribute()
-    {
-        return $this->favorites->count();
-    }
+    // public function getFavoritesCountAttribute()
+    // {
+    //     return $this->favorites->count();
+    // }
 
-    /**
-     * 保存点赞信息
-     *
-     * @return void
-     */
-    public function favorite()
-    {
-        $attributes = ['user_id' => auth()->id()];
-        if (! $this->favorites()->where($attributes)->exists()) {
-            return $this->favorites()->create($attributes);    
-        }
-    }
+    // /**
+    //  * 保存点赞信息
+    //  *
+    //  * @return void
+    //  */
+    // public function favorite()
+    // {
+    //     $attributes = ['user_id' => auth()->id()];
+    //     if (! $this->favorites()->where($attributes)->exists()) {
+    //         return $this->favorites()->create($attributes);    
+    //     }
+    // }
 
-    /**
-     * 判断是否之前点赞过
-     *
-     * @return boolean
-     */
-    public function isFavorited()
-    {
-        // return $this->favorites()->where(['user_id' => auth()->id()])->exists();
-        return !! $this->favorites->where('user_id', auth()->id())->count();
-    }
+    // /**
+    //  * 判断是否之前点赞过
+    //  *
+    //  * @return boolean
+    //  */
+    // public function isFavorited()
+    // {
+    //     // return $this->favorites()->where(['user_id' => auth()->id()])->exists();
+    //     return !! $this->favorites->where('user_id', auth()->id())->count();
+    // }
 }
