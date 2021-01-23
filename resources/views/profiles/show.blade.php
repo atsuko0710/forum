8393767822eeb0c0b6ec219c4d24e7b254d05331
lincy <lincy@wecut.com>
2021-01-23 09:34:25 +0800

    class65

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -8,6 +8,17 @@
                     <h1>
                         {{ $profileUser->name }}
                     </h1>
+
+                    @can('update', $profileUser)
+                    <form action="{{ route('avatar', $profileUser) }}" method="POST" enctype="multipart/form-data">
+                        {{ csrf_field() }}
+                        <input type="file" name="avatar">
+
+                        <button type="submit" class="btn btn-primary">Add Avatar</button>
+                    </form>
+                    @endcan
+
+                    <img src="/{{ $profileUser->avatar_path }}" width="200" height="200">
                 </div>
 
                 @forelse($activities as $date => $activity)
